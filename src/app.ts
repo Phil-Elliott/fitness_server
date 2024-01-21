a189fa8a9e53323e2bf6b0533842dd3c73ad8bdf
Phil <phillippelliott@gmail.com>
2024-01-21 22:17:51 +0800

    added clerk

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import AppError from "./utils/appError";
@@ -22,11 +23,25 @@ securityMiddleware(app);
 app.use(cookieParser());
 
 // ROUTES
-app.use("/api/v1/", routes.exerciseRoutes);
-app.use("/api/v1/", routes.routineRoutes);
-app.use("/api/v1/", routes.userRoutes);
-app.use("/api/v1/", routes.workoutExerciseRoutes);
-app.use("/api/v1/", routes.workoutRoutes);
+app.use("/api/v1/", ClerkExpressRequireAuth);
+app.use("/api/v1/user", routes.userRoutes);
+app.use("/api/v1/routine", routes.routineRoutes);
+app.use("/api/v1/workout", routes.workoutRoutes);
+app.use("/api/v1/workoutExercise", routes.workoutExerciseRoutes);
+app.use("/api/v1/exercise", routes.exerciseRoutes);
+
+// Clerk's error handler
+app.use((err: any, req: any, res: any, next: any) => {
+  if (err && err.statusCode === 401) {
+    return res.status(401).json({
+      error: {
+        name: err.name,
+        message: err.message,
+      },
+    });
+  }
+  next(err);
+});
 
 // 404 route
 app.all("*", (req, res, next) => {
