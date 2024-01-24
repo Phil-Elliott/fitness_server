58f2424a02e0b2aaad85df3f9cd87875376317b8
Phil <phillippelliott@gmail.com>
2024-01-24 13:14:07 +0800

    added all exercise routes

--- src/routes/exerciseRoutes.ts
+++ src/routes/exerciseRoutes.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import * as exerciseController from "../controllers/exerciseController";
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(exerciseController.getAllExercises)
+  .post(exerciseController.createExercise);
+
+router
+  .route("/:id")
+  .get(exerciseController.getExercise)
+  .patch(exerciseController.updateExercise)
+  .delete(exerciseController.deleteExercise);
 
 export default router;
