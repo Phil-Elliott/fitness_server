b357947c97b0386aee29df66325e635e94bcac5b
Phil <phillippelliott@gmail.com>
2024-02-01 10:23:10 +0800

    finished all exercise queries

--- src/dataAccess/exerciseDataAccess.ts
+++ src/dataAccess/exerciseDataAccess.ts
@@ -1,4 +1,7 @@
 import db from "../database/setup";
+import { exercises } from "../database/schema/exerciseSchema";
+import { sql } from "drizzle-orm";
+
 import buildUpdateQuery from "../utils/buildUpdateQuery";
 
 type Exercise = {
@@ -8,41 +11,39 @@ type Exercise = {
 };
 
 export const getAllExercises = async () => {
-  const result = await db.query("SELECT * FROM exercises");
+  const result = await db.execute(sql`SELECT * FROM exercises`);
   return result.rows;
 };
 
 export const getExercise = async (id: string) => {
-  const result = await db.query("SELECT * FROM exercises WHERE id = $1", [id]);
+  const result = await db.execute(
+    sql`SELECT * FROM exercises WHERE id = ${id}`
+  );
   return result.rows[0];
 };
 
 export const createExercise = async (exerciseData: Exercise) => {
-  const result = await db.query(
-    "INSERT INTO exercises (name, description) VALUES ($1, $2) RETURNING *",
-    [exerciseData.name, exerciseData.description]
+  const result = await db.execute(
+    sql`INSERT INTO exercises (name, description) VALUES (${exerciseData.name}, ${exerciseData.description}) RETURNING *`
   );
   return result.rows[0];
 };
 
 export const updateExercise = async (id: string, exerciseData: Exercise) => {
-  const idResult = await db.query("SELECT id FROM exercises WHERE id = $1", [
-    id,
-  ]);
+  const idResult = await db.execute(
+    sql`SELECT id FROM exercises WHERE id = ${id}`
+  );
   if (idResult.rows.length === 0) {
     throw new Error(`Exercise with ID ${id} does not exist.`);
   }
 
-  const { query, params } = buildUpdateQuery(
-    "exercises",
-    exerciseData,
-    "id",
-    id
-  );
-  const result = await db.query(query, params);
+  const updateQuery = buildUpdateQuery("exercises", exerciseData, "id", id);
+  const result = await db.execute(updateQuery());
+
   return result.rows[0];
 };
 
 export const deleteExercise = async (id: string) => {
-  return db.query("DELETE FROM exercises WHERE id = $1", [id]);
+  await db.execute(sql`DELETE FROM exercises WHERE id = ${id}`);
+  return;
 };
