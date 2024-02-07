a5d11d6bd20c78d6bb524b8374805105b1721ded
Phil <phillippelliott@gmail.com>
2024-02-07 16:07:47 +0800

    created all crud routes for routines

--- src/routes/routineRoutes.ts
+++ src/routes/routineRoutes.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import * as routineController from "../controllers/routineController";
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(routineController.getAllRoutines)
+  .post(routineController.createRoutine);
+
+router
+  .route("/:id")
+  .get(routineController.getRoutine)
+  .patch(routineController.updateRoutine)
+  .delete(routineController.deleteRoutine);
 
 export default router;
