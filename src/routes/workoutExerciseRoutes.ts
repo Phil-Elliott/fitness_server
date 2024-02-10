3d52e672e8b9e04a7f23e3d2eec940afc4596e5f
Phil <phillippelliott@gmail.com>
2024-02-10 09:01:47 +0800

    added all crud routes for workout exercises

--- src/routes/workoutExerciseRoutes.ts
+++ src/routes/workoutExerciseRoutes.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import * as workoutExerciseController from "../controllers/workoutExerciseController";
 
 const router = express.Router();
+
+router
+  .route("/")
+  .get(workoutExerciseController.getAllWorkoutExercises)
+  .post(workoutExerciseController.createWorkoutExercise);
+
+router
+  .route("/:id")
+  .get(workoutExerciseController.getWorkoutExercise)
+  .patch(workoutExerciseController.updateWorkoutExercise)
+  .delete(workoutExerciseController.deleteWorkoutExercise);
 
 export default router;
