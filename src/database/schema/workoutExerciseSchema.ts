99dd8fcdb3b1d60273262f837c781f9ef511b358
Phil <phillippelliott@gmail.com>
2024-02-10 12:50:36 +0800

    moved workoutExerciseSet schema to its own file

--- src/database/schema/workoutExerciseSchema.ts
+++ src/database/schema/workoutExerciseSchema.ts
@@ -12,16 +12,3 @@ export const workoutExercises = pgTable("workoutExercises", {
     .notNull(),
   order_index: integer("order_index"),
 });
-
-export const workoutExerciseSets = pgTable("workoutExerciseSets", {
-  id: serial("id").primaryKey(),
-  workout_exercise_id: integer("workout_exercise_id")
-    .references(() => workoutExercises.id)
-    .notNull(),
-  set_number: integer("set_number"),
-  repetitions: integer("repetitions"),
-  weight: integer("weight"),
-  weight_unit: varchar("weight_unit", { length: 256 }),
-  user_input: varchar("user_input", { length: 256 }),
-  created_at: varchar("created_at", { length: 256 }),
-});
