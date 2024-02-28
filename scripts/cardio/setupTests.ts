0d44120c14e1e9d3497b9928ce4c2a3fb7fd3090
Phil <phillippelliott@gmail.com>
2024-02-28 14:44:09 +0800

    added scripts for cardio tests

---
+++ scripts/cardio/setupTests.ts
@@ -0,0 +1,64 @@
+import { sql } from "drizzle-orm";
+import db from "../../src/database/setup";
+
+async function setupWorkoutTableForTestDatabase() {
+  try {
+    await db.execute(
+      sql`INSERT INTO users (id, email, display_name) VALUES ('user_12543', 'johndoe@gmail.com', 'John Doe');`
+    );
+
+    const routine = await db.execute(
+      sql`INSERT INTO routines (user_id, name, notes, frequency,  start_date, end_date, created_at) VALUES ('user_12543', 'Morning Routine', 'A routine to start the day', 'daily',  date '2025-09-20', date '2025-11-12', NOW()) RETURNING *;`
+    );
+
+    const routineId = routine.rows[0].id;
+
+    const cardioExercise = await db.execute(
+      sql`INSERT INTO "cardioExercises" (name, description) VALUES ('Run', 'Run fast') RETURNING *;`
+    );
+
+    const cardioExerciseId = cardioExercise.rows[0].id;
+
+    await db.execute(
+      sql`INSERT INTO cardio (user_id, routine_id, cardio_exercise_id, duration, distance, date, notes) VALUES ('user_12543', ${routineId}, ${cardioExerciseId}, 30, 5, date '2024-02-26', 'A cardio to start the day');`
+    );
+    await db.execute(
+      sql`INSERT INTO cardio (user_id, routine_id, cardio_exercise_id, duration, distance, date, notes) VALUES ('user_12543', ${routineId}, ${cardioExerciseId}, 45, 10, date '2024-02-29', 'A cardio to end the day');`
+    );
+    await db.execute(
+      sql`INSERT INTO cardio (user_id, routine_id, cardio_exercise_id, duration, distance, date, notes) VALUES ('user_12543', ${routineId}, ${cardioExerciseId}, 60, 15, date '2024-01-18', 'A cardio to break up the day');`
+    );
+  } catch (error) {
+    console.error("Error setting up cardio table in test database:", error);
+  }
+}
+
+export default setupWorkoutTableForTestDatabase;
+
+/*
+- use same user id
+- create a routine and get the routine id
+- create a cardio exercise and get the cardio exercise id
+- create some cardio items
+- also create a cardio item with a different user id
+
+
+*/
+
+// export const cardio = pgTable("cardio", {
+//   id: serial("id").primaryKey(),
+//   user_id: varchar("user_id", { length: 256 })
+//     .references(() => users.id, { onDelete: "cascade" })
+//     .notNull(),
+//   routine_id: integer("routine_id").references(() => routines.id, {
+//     onDelete: "cascade",
+//   }),
+//   cardio_exercise_id: integer("cardio_exercise_id").references(
+//     () => cardioExercises.id
+//   ),
+//   duration: integer("duration"),
+//   distance: integer("distance"),
+//   date: date("date").notNull(),
+//   notes: text("notes"),
+//   created_at: timestamp("created_at").defaultNow(),
+// });
---
+++ scripts/cardio/teardownTests.ts
@@ -0,0 +1,18 @@
+import { sql } from "drizzle-orm";
+import db from "../../src/database/setup";
+
+async function teardownWorkoutTableForTestDatabase() {
+  try {
+    await db.execute(sql`DELETE FROM cardio`);
+    await db.execute(sql`DELETE FROM "cardioExercises"`);
+    await db.execute(sql`DELETE FROM routines`);
+    await db.execute(sql`DELETE FROM users`);
+  } catch (error) {
+    console.error(
+      "Error tearing down cardio table in test database workout:",
+      error
+    );
+  }
+}
+
+export default teardownWorkoutTableForTestDatabase;
