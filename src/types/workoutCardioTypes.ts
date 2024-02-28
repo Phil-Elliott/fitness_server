22169b6ea6722b886d479aff844b243b530890d6
Phil <phillippelliott@gmail.com>
2024-02-28 17:18:50 +0800

    added workoutCardio types

---
+++ src/types/workoutCardioTypes.ts
@@ -0,0 +1,12 @@
+export type WorkoutCardio = {
+  id: number;
+  cardio_id: number;
+  workout_id: number;
+  order_index: number;
+};
+
+export type NewWorkoutCardio = {
+  cardio_id: number;
+  workout_id: number;
+  order_index: number;
+};
