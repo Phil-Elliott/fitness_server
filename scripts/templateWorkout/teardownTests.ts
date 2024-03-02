58577d1f8b4db1940670d7b53ff2495170029410
Phil <phillippelliott@gmail.com>
2024-03-02 13:20:35 +0800

    added scripts for tests

--- scripts/templateWorkout/teardownTests.ts
+++ scripts/templateWorkout/teardownTests.ts
@@ -3,7 +3,7 @@ import db from "../../src/database/setup";
 
 async function teardownTemplateWorkoutTableForTestDatabase() {
   try {
-    await db.execute(sql`DELETE FROM templateWorkouts`);
+    await db.execute(sql`DELETE FROM "templateWorkouts"`);
     await db.execute(sql`DELETE FROM routines`);
     await db.execute(sql`DELETE FROM users`);
   } catch (error) {
---
+++ scripts/templateWorkoutExercise/setupTests.ts
@@ -0,0 +1,57 @@
+import { sql } from "drizzle-orm";
+import db from "../../src/database/setup";
+
+async function setupTemplateWorkoutExerciseTableForTestDatabase() {
+  try {
+    await db.execute(
+      sql`INSERT INTO users (id, email, display_name) VALUES ('user_12543', 'johndoe@gmail.com', 'John Doe');`
+    );
+
+    const routine = await db.execute(
+      sql`INSERT INTO routines (user_id, name, notes, frequency,  start_date, end_date, created_at) VALUES ('user_12543', 'Morning Routine', 'A routine to start the day', 'daily',  date '2025-09-20', date '2025-11-12', NOW()) RETURNING *;`
+    );
+
+    const routineId = routine.rows[0].id;
+
+    const workout = await db.execute(
+      sql`INSERT INTO workouts (user_id, routine_id, name, notes, date, workout_status, created_at) VALUES ('user_12543', ${routineId}, 'Morning Workout', 'A workout to start the day', date '2024-02-26', 'incomplete', NOW()) RETURNING *`
+    );
+
+    const workoutId = workout.rows[0].id;
+
+    const exercise = await db.execute(
+      sql`INSERT INTO exercises (name, description) VALUES ('Push Ups', 'A classic exercise to build upper body strength') RETURNING *`
+    );
+    const exerciseId = exercise.rows[0].id as number;
+
+    await db.execute(
+      sql`INSERT INTO "templateWorkoutExercises" (workout_id, exercise_id, order_index) VALUES (${workoutId}, ${exerciseId}, 1);`
+    );
+    await db.execute(
+      sql`INSERT INTO "templateWorkoutExercises" (workout_id, exercise_id, order_index) VALUES (${workoutId}, ${exerciseId}, 2);`
+    );
+    await db.execute(
+      sql`INSERT INTO "templateWorkoutExercises" (workout_id, exercise_id, order_index) VALUES (${workoutId}, ${exerciseId}, 3);`
+    );
+  } catch (error) {
+    console.error(
+      "Error setting up WorkoutExercise table in test database:",
+      error
+    );
+  }
+}
+
+export default setupTemplateWorkoutExerciseTableForTestDatabase;
+
+// export const templateWorkoutExercises = pgTable("templateWorkoutExercises", {
+//   id: serial("id").primaryKey(),
+//   template_workout_id: integer("template_workout_id")
+//     .references(() => templateWorkouts.id, { onDelete: "cascade" })
+//     .notNull(),
+//   exercise_id: integer("exercise_id")
+//     .references(() => exercises.id)
+//     .notNull(),
+//   order_index: integer("order_index").notNull(),
+//   sets: integer("sets").notNull(),
+//   rest_between_sets: integer("rest_between_sets").notNull(),
+// });
